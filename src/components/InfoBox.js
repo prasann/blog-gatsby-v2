8a42d900e01ff408f69cd5417ad2233903010588
Prasanna Venkatesan <mail.prasanna.v@gmail.com>
2020-07-05 00:43:13 +0530

    Started with layouts

---
+++ src/components/InfoBox.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const InfoBox = () => {
+    return <div>I'm the info box</div>
+}
+
+export default InfoBox;
---
+++ src/components/Navigator.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Navigator = () => {
+    return <div>I'm the navigator box</div>
+}
+
+export default Navigator;
